d6b898d9178002ae259983166b44cd4cfca79c96
avinight <meneses.frederick9@gmail.com>
2025-12-16 01:39:48 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [icon()],
   adapter: cloudflare()
 });
