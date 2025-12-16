a7a4acbc250079ffa727095fe465c6d768a31fac
avinight <meneses.frederick9@gmail.com>
2025-12-16 01:54:47 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
+	integrations: [icon()],
+	output: 'static'
 });
