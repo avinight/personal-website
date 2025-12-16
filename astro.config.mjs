1037e96c8a793107d4cba76cab4ff70eb20e544e
avinight <meneses.frederick9@gmail.com>
2025-12-16 01:41:42 -0500

    Remove Cloudflare adapter for static Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,7 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  integrations: [icon()],
-  adapter: cloudflare()
+  integrations: [icon()]
 });
